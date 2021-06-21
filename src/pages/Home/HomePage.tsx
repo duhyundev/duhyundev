2038343500504a4bfaede14ec26848cb33af0a66
duhyunkim <duhyun.dev@gmail.com>
2021-06-21 23:56:55 +0900

    DHK-7 : list 컴포넌트 구현

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,9 +1,35 @@
 import './HomePage.scss';
 import { OnlyLeftSideBarLayout } from '@layouts';
+import Activity from '@assets/icons/activity.svg';
 
 const HomePage = () => {
   return (
-    <OnlyLeftSideBarLayout sidebar={<div></div>}>
+    <OnlyLeftSideBarLayout
+      sidebar={
+        <div className="list-group">
+          <div className="list-active">
+            <img src={Activity} />
+            <span>홈</span>
+            <img src={Activity} />
+          </div>
+          <div className="list">
+            <img src={Activity} />
+            <span>스토리</span>
+            <img src={Activity} />
+          </div>
+          <div className="list">
+            <img src={Activity} />
+            <span>포스팅</span>
+            <img src={Activity} />
+          </div>
+          <div className="list-inactive">
+            <img src={Activity} />
+            <span>네트워크</span>
+            <img src={Activity} />
+          </div>
+        </div>
+      }
+    >
       <div className="HomePage"></div>
     </OnlyLeftSideBarLayout>
   );
