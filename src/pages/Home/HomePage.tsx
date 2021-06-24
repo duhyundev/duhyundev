7ce8de2f694b270fc8f22dd6ccdb70a53a72da8f
duhyunkim <duhyun.dev@gmail.com>
2021-06-24 23:54:07 +0900

    DHK-7 : 사이드바 목업 구현

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,31 +1,64 @@
 import './HomePage.scss';
 import { OnlyLeftSideBarLayout } from '@layouts';
 import Activity from '@assets/icons/activity.svg';
+import { ReactComponent as MenuIcon } from '@assets/icons/menu.svg';
+import HomeIcon from '@assets/icons/home.svg';
+import BookIcon from '@assets/icons/book.svg';
+import PeopleIcon from '@assets/icons/people.svg';
+import DevonLane from '@assets/icons/UserPic/devon-lane.svg';
+import GoogleIcon from '@assets/icons/google.svg';
+import MessageCircleIcon from '@assets/icons/message-circle.svg';
+import priceTagIcon from '@assets/icons/price-tag.svg';
 
 const HomePage = () => {
   return (
     <OnlyLeftSideBarLayout
       sidebar={
-        <div className="list-group">
-          <div className="list-active">
-            <img src={Activity} />
-            <span>홈</span>
-            <img src={Activity} />
+        <div className="sidebarComponents">
+          <div className="account">
+            <img className="picture-user" src={DevonLane} />
+            <section>
+              <div className="welcome">안녕하세요</div>
+              <div className="username">낯선사람51345</div>
+            </section>
+            <button>
+              <img src={GoogleIcon} />
+            </button>
           </div>
-          <div className="list">
-            <img src={Activity} />
-            <span>스토리</span>
-            <img src={Activity} />
-          </div>
-          <div className="list">
-            <img src={Activity} />
-            <span>포스팅</span>
-            <img src={Activity} />
-          </div>
-          <div className="list-inactive">
-            <img src={Activity} />
-            <span>네트워크</span>
-            <img src={Activity} />
+          <section className="body">
+            <div className="list-group">
+              <div className="list-active">
+                <img src={HomeIcon} />
+                <span>홈</span>
+                <MenuIcon />
+              </div>
+              <div className="list">
+                <img src={Activity} />
+                <span>스토리</span>
+                <MenuIcon />
+              </div>
+              <div className="list">
+                <img src={BookIcon} />
+                <span>포스팅</span>
+                <MenuIcon />
+              </div>
+              <div className="list-inactive">
+                <img src={PeopleIcon} />
+                <span>네트워크</span>
+                <MenuIcon />
+              </div>
+            </div>
+          </section>
+
+          <div className="footer">
+            <div className="list">
+              <img src={priceTagIcon} />
+              <span>라이센스 & 저작권</span>
+            </div>
+            <div className="list">
+              <img src={MessageCircleIcon} />
+              <span>문의하기</span>
+            </div>
           </div>
         </div>
       }
